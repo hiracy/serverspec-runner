73fe1173c663b1d6045be6ad95829d36ff925840
hiracy <leizhen@mbr.nifty.com>
2014-06-24 02:37:54 +0900

    add default example test

--- spec/example/default.rb
+++ spec/example/default.rb
@@ -3,7 +3,7 @@ require "#{File.dirname(__FILE__)}/../spec_helper"
 describe "example" do
 
   describe "hostname実行" do
-    describe command("expect -c \'set timeout 5; spawn sudo /usr/sbin/tcpdump -i eth2 -n vrrp; expect eof\' | grep 'vrid 999'") do
+    describe command("hostname") do
       it { return_exit_status 0 }
     end
   end
